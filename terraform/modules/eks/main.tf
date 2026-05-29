24d76b751572559dfedf01d5287cdad40e274406
vasupateljsk089-byte <vasupateljsk089@gmail.com>
2026-05-29 15:14:49 +0530

    Update EKs

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -33,8 +33,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 2
-      max_size     = 3
-      desired_size = 2
+      max_size     = 4
+      desired_size = 3
 
       key_name = var.key_name
     }
